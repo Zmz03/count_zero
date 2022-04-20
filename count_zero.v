63a777006f8580c26fd95dfc3815182d89caf249
Zmz03 <z1521174099@gmail.com>
2022-04-20 10:32:54 +0800

    组合逻辑实现，输入四位数据，并且没有参数化

--- count_zero.v
+++ count_zero.v
@@ -4,46 +4,27 @@
 //description: count the number of 0 before 1 in one data, for example count 0010_1001 is 2.
 
 module count_zero (
-    clk,
     i_data,
     o
 );
 
-parameter width = 8;
 
-input clk;
-input  [width   - 1  : 0] i_data;
+input  [3  : 0] i_data;
 
-output reg [width/2 - 1  : 0] o;
+output reg [3 : 0] o;
 
-wire   [width   - 1  : 0] i_data_n;
-reg    [width   - 1  : 0] i = 0;
-reg    [width   - 1  : 0] data_t;
+wire   [3  : 0] i_data_n;
+reg    [3  : 0] data_t;
 
 assign i_data_n = ~i_data;
 
-always @(posedge clk)begin
-    if(i == 0)begin
-        data_t[i] = i_data_n[i];
-        i = i + 1;       
-    end
-    else if(i <= width - 1)begin
-        data_t[i] = data_t[i-1] & i_data_n[i];
-        i = i + 1;       
-    end
-    else
-        i = 0;
-end
+always @(*)begin
+    data_t[0] = i_data_n[0];
+    data_t[1] = i_data_n[1] & data_t[0];
+    data_t[2] = i_data_n[2] & data_t[1];
+    data_t[3] = i_data_n[3] & data_t[2];
 
-always @(posedge clk) begin
-    if(i <= width - 1)begin
-        o = o + data_t[i]; 
-        i = i + 1;       
-    end
-    else begin
-        o = 0;
-        i = 0;
-    end
+    o = data_t[0] + data_t[1] + data_t[2] + data_t[3];
 end
     
 endmodule
--- count_zero_tb.v
+++ count_zero_tb.v
@@ -2,24 +2,18 @@
 
 module count_zero_tb();
 
-parameter width = 8;
-
-reg clk;
-reg  [width -1: 0] i_data;
-wire [width/2 -1: 0] o;
+reg  [3 : 0] i_data;
+wire [3 : 0] o;
 
 count_zero count_zero_0(
-    .clk(clk),
     .i_data(i_data),
     .o(o)
 );
 
 initial begin
-    clk = 0;
     i_data = 0;
 end
 
-always #20 clk = ~clk;
 always #40 i_data = i_data + 1;
 
 
